69449a80c116c1986b5c954d704a99b253575d6e
SagarPradhan613 <karanpradhan42@gmail.com>
2024-03-15 19:30:43 +0530

    favicon

--- src/app/page.js
+++ src/app/page.js
@@ -43,7 +43,10 @@ export default function Home() {
   return (
     <>
       <Head>
-        <link rel="shortcut icon" href="/Images/favicon.ico" />
+        <link rel="shortcut icon" href="/images/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png" />
       </Head>
 
       <Modal isOpen={isModalOpen} handleToggleModal={handleToggleModal} onClose={handleCloseModal} openPrivacy={openPrivacy} setOpenPrivacy={setOpenPrivacy}></Modal>
